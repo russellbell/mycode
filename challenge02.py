ad0bf046ffafb6db3b4db04478032cf0ce8792e2
russellbell <42256548+russellbell@users.noreply.github.com>
2020-05-08 10:05:04 -0400

    just saving work

--- challenge02.py
+++ challenge02.py
@@ -7,8 +7,8 @@ In 1492, Columbus sailed the ocean blue. <name> fell off the boat
 """
 col_list = ["blue", "Columbus"]
 col_list.append(1492)
-#print(col_list)
+
 username = input("Please enter your name: ")
 print(f"In {col_list[2]}, {col_list[1]} sailed the ocean {col_list[0]}. {username} fell off the boat.")
 
-print(type(col_list[2]))
+
